214d97c5a12bf2dcf84bfdeb39e4c72e6abbfc0c
Viktorvallmark <vallmark.viktor@gmail.com>
2023-03-12 19:42:04 +0100

    Second commit

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Shop() {
+    return(
+        <div>
+            <h1>
+                Welcome to my shop!
+            </h1>
+
+            <button type="submit">Log out</button>
+        </div>
+    );
+}
+
+export default Shop;
